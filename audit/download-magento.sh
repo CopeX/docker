ba9ff9f41382dda73887f947e8dec5284c563ae2
Andreas Pointner <andreaspointner@gmail.com>
2016-02-05 18:50:07 +0100

    [TASK] Bugfix in audit module because of repository change

--- audit/download-magento.sh
+++ audit/download-magento.sh
@@ -16,4 +16,4 @@ echo "Downloading Magento " $MAGE_VERSION
 echo $MAGE_URL
 
 mkdir -p $DEST \
-    && wget -qO- $MAGE_URL | tar -zxf- -C $DEST --strip=1 magento-mirror-$MAGE_VERSION
+    && wget -qO- $MAGE_URL | tar -zxf- -C $DEST --strip=1 magento-lts-$MAGE_VERSION
