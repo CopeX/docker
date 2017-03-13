5c500d39a68f359274e0cad5de96c519a789bc02
Andreas Pointner <andreaspointner@gmail.com>
2017-03-13 18:58:50 +0100

    [TASK] Added PHP 5.6 and PHP 7.1 Support with environment variable PHP_VERSION

--- nginx-php-fpm/php-fpm/service/runit.sh
+++ nginx-php-fpm/php-fpm/service/runit.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-php5-fpm -c /etc/php5/fpm
+if [ -z "$PHP_VERSION" ]; then
+    PHP_VERSION="5.6"
+fi
+sed -i -e "s/\/var\/run\/php5\.6-fpm\.sock/\/var\/run\/php$PHP_VERSION-fpm\.sock/" /etc/nginx/conf.d/includes/php/10-params.conf
+`php-fpm$PHP_VERSION` -c /etc/php/$PHP_VERSION/fpm
