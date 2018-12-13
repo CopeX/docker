cb4c15bd0c24731409511b4c6ff292f4fb5314cb
Andreas Pointner <andreas.pointner@copex.io>
2018-12-13 18:59:52 +0100

    [TASK] Changed output to display error if no config is set

--- satis/bin/build.sh
+++ satis/bin/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-cd /build
-/satis/bin/satis build /satis/config/satis.json
+if [ -f /satis/config/satis.json ]; then
+    mkdir -p /build
+    cd /build
+    /satis/bin/satis build /satis/config/satis.json
+else
+    echo "No satis config found"
+fi
