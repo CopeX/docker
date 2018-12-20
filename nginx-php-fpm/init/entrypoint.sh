bda631bf92eca6d55e96b0b0aa22492b19f22475
Andreas Pointner <andreas.pointner@copex.io>
2018-12-20 11:02:11 +0100

    [TASK] Added M2 default support and blackfire settings

--- nginx-php-fpm/init/entrypoint.sh
+++ nginx-php-fpm/init/entrypoint.sh
@@ -27,9 +27,12 @@ if [ -z ${MAGENTO_VERSION:-} ]; then
     MAGENTO_VERSION="1"
 fi
 
-if [ "MAGENTO_VERSION" = "2" ]; then
-    mv /etc/nginx/sites-templates/default.conf.tmpl /etc/nginx/sites-templates/defaultM1.conf.tmpl
-    mv /etc/nginx/sites-templates/defaultM2.conf.tmpl /etc/nginx/sites-templates/default.conf.tmpl
+if [ "$MAGENTO_VERSION" = "2" ]; then
+    if [ -e /etc/nginx/sites-templates/defaultM2.conf.tmpl ]; then
+        mkdir /etc/nginx/sites-templates/M1
+        mv /etc/nginx/sites-templates/default.conf.tmpl /etc/nginx/sites-templates/M1/default.conf.tmpl
+        mv /etc/nginx/sites-templates/defaultM2.conf.tmpl /etc/nginx/sites-templates/default.conf.tmpl
+    fi
 fi
 
 #clean
@@ -67,4 +70,27 @@ chmod +x /etc/service/php-fpm-noxdebug/run
 if [ "$PHP_ENABLE_XDEBUG" = "false" ]; then
     rm -f /etc/php/$PHP_VERSION/fpm/conf.d/20-xdebug.ini
     rm -f /etc/php/$PHP_VERSION/cli/conf.d/20-xdebug.ini
+fi
+
+if [ -z ${ENABLE_BLACKFIRE:-} ]; then
+    ENABLE_BLACKFIRE="false"
+fi
+
+if [ "$ENABLE_BLACKFIRE" = "true" ]; then
+    php_versions=(5.6 7.0 7.1 7.2 7.3)
+    for version in "${php_versions[@]}"
+    do
+        if [ ! -e /etc/php/$version/mods-available/blackfire.ini ]; then
+            downloadLink="https://blackfire.io/api/v1/releases/probe/php/linux/amd64/${version//.}"
+            echo "Downloading blackfire for PHP $version from $downloadLink"
+            curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -L -s $downloadLink
+            mkdir -p /tmp/blackfire
+            tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire
+            mv /tmp/blackfire/blackfire-*.so $(php$version -r "echo ini_get('extension_dir');")/blackfire.so
+            printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > /etc/php/$version/mods-available/blackfire.ini
+            ln -s /etc/php/$version/mods-available/blackfire.ini /etc/php/$version/cli/conf.d/30-blackfire.ini
+            ln -s /etc/php/$version/mods-available/blackfire.ini /etc/php/$version/fpm/conf.d/30-blackfire.ini
+            rm -rf /tmp/blackfire /tmp/blackfire-probe.tar.gz
+        fi
+    done
 fi
