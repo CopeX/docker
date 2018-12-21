a8b6d6e209b935b03b1b21aad6ffed71c1ad370a
Andreas Pointner <andreas.pointner@copex.io>
2018-12-21 14:05:35 +0100

    [TASK] separated entrypoint files

--- nginx-php-fpm/init/30_debug.sh
+++ nginx-php-fpm/init/30_debug.sh
@@ -1,61 +1,9 @@
 #!/bin/bash
 
-set -eu
-
-
-templates_dir="/etc/nginx/sites-templates"
-outdir="/etc/nginx/conf.d"
-
-function template_files() {
-    find "${templates_dir}" \
-        -mindepth 1 \
-        -maxdepth 1 \
-        -name '*.tmpl' \
-        -print0
-}
-
-function non_template_files() {
-    find "${templates_dir}" \
-        -mindepth 1 \
-        -maxdepth 1 \
-        -not \
-        -name '*.tmpl' \
-        -print0
-}
-
-if [ -z ${MAGENTO_VERSION:-} ]; then
-    MAGENTO_VERSION="1"
-fi
-
-if [ "$MAGENTO_VERSION" = "2" ]; then
-    if [ -e /etc/nginx/sites-templates/defaultM2.conf.tmpl ]; then
-        mkdir /etc/nginx/sites-templates/M1
-        mv /etc/nginx/sites-templates/default.conf.tmpl /etc/nginx/sites-templates/M1/default.conf.tmpl
-        mv /etc/nginx/sites-templates/defaultM2.conf.tmpl /etc/nginx/sites-templates/default.conf.tmpl
-    fi
-fi
-
-#clean
-find "${outdir}" -maxdepth 1 -type f -exec rm -v {} \;
-
-template_files | xargs -0 substitute-env-vars.sh "${outdir}"
-non_template_files | xargs -0 -I{} ln -s {} "${outdir}"
-
-if [ -z ${PHP_VERSION:-} ]; then
-    PHP_VERSION="5.6"
-fi
-ln -sf /usr/bin/php$PHP_VERSION /etc/alternatives/php
-
-echo "\n127.0.0.1 $DOMAIN" >> /etc/hosts
-
-sed -i "s/hostname=.*/hostname$DOMAIN/g" /etc/ssmtp/ssmtp.conf
-
 
 if [ -z ${PHP_ENABLE_XDEBUG:-} ]; then
     PHP_ENABLE_XDEBUG="true"
 fi
-
-
 
 # No Xdebug fpm
 cp -r /etc/php/$PHP_VERSION /etc/php/$PHP_VERSION-noxdebug
