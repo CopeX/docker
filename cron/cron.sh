223bc7eb5f8930c01fbfc9c93e853a386f841e9c
Andreas Pointner <andreas.pointner@copex.io>
2019-01-03 09:48:13 +0100

    [TASK] Updated cron container to work with magento 2

--- cron/cron.sh
+++ cron/cron.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-if [ -e $MAGENTO_ROOT/scheduler_cron.sh ]; then
-    echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode always" > mycron
-    echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode default" >> mycron
-else
-    echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/sh $MAGENTO_ROOT/cron.sh" > mycron
+if [ -z ${MAGENTO_VERSION:-} ]; then
+    MAGENTO_VERSION="1"
 fi
 
+if [ $MAGENTO_VERSION = 2 ]; then
+    echo "* * * * * php $MAGENTO_ROOT/bin/magento cron:run >> $MAGENTO_ROOT/var/log/cron_run.log" > mycron
+else
+    if [ -e $MAGENTO_ROOT/scheduler_cron.sh ]; then
+        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode always" > mycron
+        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode default" >> mycron
+    else
+        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/sh $MAGENTO_ROOT/cron.sh" > mycron
+    fi
+fi
 crontab -u www-data mycron
 
 rm mycron
