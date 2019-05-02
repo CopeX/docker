2ad17908ef193266288dc82783835926cb0f5a1c
Andreas Pointner <andreas.pointner@copex.io>
2019-05-02 20:07:12 +0200

    [TASK] Update Satis Repo to use date to expire

--- satis/bin/docker-entrypoint.sh
+++ satis/bin/docker-entrypoint.sh
@@ -15,11 +15,20 @@ for _DOMAIN in $PRIVATE_REPO_DOMAIN_LIST ; do
         ssh-keyscan -t rsa,dsa $_DOMAIN >> /root/.ssh/known_hosts
     fi
 done
-if [ ! -f /tmp/init ]; then
+
+if [ -z ${UPDATE_EXPIRES:-} ]; then
+    echo "No UPDATE_EXPIRES given! Exit!"
+    exit
+fi
+
+if [[ ! -f /tmp/init ]]; then
     echo "Initializing crontab"
-    env UPDATE_PERIOD="$UPDATE_PERIOD" php /satis/bin/crontab.php | crontab -
+    env UPDATE_EXPIRES="${UPDATE_EXPIRES}" php /satis/bin/crontab.php | crontab -
+    echo "Setting at to ${UPDATE_EXPIRES}"
+    at -f /satis/bin/clear.sh ${UPDATE_EXPIRES}
     touch /tmp/init
 fi
+
 /satis/bin/build.sh
 
 exec /usr/bin/supervisord -n -c/etc/supervisor/supervisord.conf
