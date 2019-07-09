7c88b026b797147dae8fb108ee39af5fe0c94e30
Andreas Pointner <andreas.pointner@copex.io>
2019-07-09 15:37:04 +0200

    Revert "[TASK] Symlink php.ini only on startup to allow updates"
    This reverts commit a83b347c54c03983309e8e7f25b73f67f1e758df.

--- php/init/20_set_php_version.sh
+++ php/init/20_set_php_version.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-ln -sf /etc/php/php.ini /etc/php/5.6/cli/php.ini && \
-ln -sf /etc/php/php.ini /etc/php/7.0/cli/php.ini && \
-ln -sf /etc/php/php.ini /etc/php/7.1/cli/php.ini && \
-ln -sf /etc/php/php.ini /etc/php/7.2/cli/php.ini && \
-ln -sf /etc/php/php.ini /etc/php/7.3/cli/php.ini
-
 # Set PHP-Version
 if [ -z ${PHP_VERSION:-} ]; then
     PHP_VERSION="5.6"
