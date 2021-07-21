b634ecca2c0af8d84077bc730b906e987a15cc8f
Andreas Pointner <andreas.pointner@copex.io>
2021-07-21 15:58:42 +0200

    [TASK] Fixing memory limit issue

--- php/init/22_memory_limit.sh
+++ php/init/22_memory_limit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Set memory limit
-if [[ -z ${MEMORY_LIMIT} ]]; then
+if [[ -n "${MEMORY_LIMIT}" ]]; then
     sed -i "s/memory_limit = .*/memory_limit = $MEMORY_LIMIT/g" /etc/php/php.ini
 fi
