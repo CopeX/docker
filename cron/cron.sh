180431832878eb606f771a7f1384c53d3a328e70
Andreas Pointner <andreas.pointner@copex.io>
2022-11-07 12:33:09 +0100

    [TASK] croncontainer with custom cron command

--- cron/cron.sh
+++ cron/cron.sh
@@ -4,14 +4,18 @@ if [ -z ${MAGENTO_VERSION:-} ]; then
     MAGENTO_VERSION="1"
 fi
 
+if [ -z "${CRON_EXPR:-}" ]; then
+    CRON_EXPR="* * * * *"
+fi
+
 if [ $MAGENTO_VERSION = 2 ]; then
-    echo "* * * * * cd $MAGENTO_ROOT && php bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> $MAGENTO_ROOT/var/log/cron_run.log" > mycron
+    echo "$CRON_EXPR cd $MAGENTO_ROOT && php bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> $MAGENTO_ROOT/var/log/cron_run.log" > mycron
 else
     if [ -e $MAGENTO_ROOT/scheduler_cron.sh ]; then
-        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode always" > mycron
-        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode default" >> mycron
+        echo "$CRON_EXPR ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode always" > mycron
+        echo "$CRON_EXPR ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/bash $MAGENTO_ROOT/scheduler_cron.sh --mode default" >> mycron
     else
-        echo "* * * * * ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/sh $MAGENTO_ROOT/cron.sh" > mycron
+        echo "$CRON_EXPR ! test -e $MAGENTO_ROOT/maintenance.flag && /bin/sh $MAGENTO_ROOT/cron.sh" > mycron
     fi
 fi
 crontab -u www-data mycron
